dd322389bfedf9a8dc2b4559d8d44b1737e8a072
PeoneEr <peoneerko@yandex.ru>
2016-04-07 16:18:51 +0600

    update autoparser

--- autoparser.rb
+++ autoparser.rb
@@ -1,40 +1,67 @@
 require 'open-uri'
 require 'nokogiri'
 require 'json'
-fname = "marks_json.txt"
 
-somefile = File.open(fname, "w")
-url_start = 'https://auto.ru'
-html = open(url_start)
-doc = Nokogiri::HTML(html)
-start_time = Time.now
-hash = {}
+class AutoParser
+  attr_reader :filename, :url, :results, :start_time,
+    :marks, :models
 
-doc.css('.mmm__item').each do |marka|
-  key = marka.children.first.children.first.text
-  value = marka.children.first["href"]
-  hash["#{key}"] = value
-end
-
-count = hash.count
-puts "Count of model = #{count}"
-resulted_hash = {}
-
-hash.each_with_index do |h, index|
-  key, value = h
-  index += 1
-  puts "Parsed #{index} model, called #{key.capitalize}, remaining #{count - index} models"
-  url = "https:#{value}"
-  html = open(url)
-  doc = Nokogiri::HTML(html)
-  models_array = []
-  doc.css('.mmm__item').each do |model|
-    models_array.push(model.children.first.text)
+  def initialize(args = {})
+    @filename = args[:filename]
+    @url = args[:url]
+    @results = {}
   end
 
-  resulted_hash["#{key}"] =  models_array.uniq
+  def do_parse
+    parse_marks
+    parse_models
+    write_to_file
+  end
+
+  private
+  def parse_marks
+    doc = Nokogiri::HTML(open(url))
+    @start_time = Time.now
+
+    {}.tap do |hash|
+      doc.css('.mmm__item').each do |marka|
+        key = marka.children.first.children.first.text
+        value = marka.children.first["href"]
+
+        hash[key] = value
+      end
+
+      @marks = hash
+    end
+
+    puts "Count of model = #{marks.count}"
+  end
+
+  def parse_models
+    marks.each_with_index do |mark, index|
+      key, value = mark
+      index += 1
+      puts "Parsed #{index} model, called #{key.capitalize}, remaining #{marks.count - index} models"
+      @url = "https:#{value}"
+      doc = Nokogiri::HTML(open(url))
+
+      [].tap do |models|
+        doc.css('.mmm__item').each do |model|
+          models << model.children.first.text
+        end
+
+        @results[key] = models
+      end
+    end
+
+    puts "Parsed for #{(Time.now - start_time).round} seconds"
+  end
+
+  def write_to_file
+    file_descriptor = File.open(filename, 'w')
+    file_descriptor.puts results.to_json
+    file_descriptor.close
+  end
 end
 
-somefile.puts resulted_hash.to_json
-puts "Parsed for #{(Time.now - start_time).round} seconds"
-somefile.close
+AutoParser.new(filename: 'result.json', url: 'https://auto.ru').do_parse
